36dce1a511edb32fde6853e899ced4c626f9bc99
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2022-11-04 14:53:51 -0300

    feat: use auth provider on app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@
 import Router from './routes';
 // theme
 import ThemeProvider from './theme';
+import {AuthProvider} from './hooks/AuthProvider'
 // components
 import ScrollToTop from './components/scroll-to-top';
-import { StyledChart } from './components/chart';
+import {StyledChart} from './components/chart';
 
 // ----------------------------------------------------------------------
 
 export default function App() {
-  return (
-    <ThemeProvider>
-      <ScrollToTop />
-      <StyledChart />
-      <Router />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <AuthProvider>
+                <ScrollToTop/>
+                <StyledChart/>
+                <Router/>
+            </AuthProvider>
+        </ThemeProvider>
+    );
 }
