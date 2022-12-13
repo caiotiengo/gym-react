8b6c9cb0cb8a95e708505908baa5d906b5945906
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2022-12-13 10:37:38 -0300

    chore: resolve welcome message

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -65,7 +65,7 @@ export default function LoginPage() {
         {mdUp && (
           <StyledSection>
             <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back
+              Olá, Bom ter você de volta!
             </Typography>
             <img src="/assets/illustrations/illustration_login.png" alt="login" />
           </StyledSection>
