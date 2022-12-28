41fe8460b66bb038e0573f4f3d80bf38b9db612e
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2022-12-28 16:54:27 -0300

    feat: clean project

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 // @mui
-import { styled, alpha } from '@mui/material/styles';
-import { Box, Link, Drawer, Typography, Avatar } from '@mui/material';
+import {styled, alpha} from '@mui/material/styles';
+import {Box, Link, Drawer, Typography, Avatar} from '@mui/material';
 // mock
-import account from '../../../_mock/account';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 // hooks
 import useResponsive from '../../../hooks/useResponsive';
 // components
@@ -20,7 +20,7 @@ import {useAuth} from "../../../hooks/useAuth";
 
 const NAV_WIDTH = 280;
 
-const StyledAccount = styled('div')(({ theme }) => ({
+const StyledAccount = styled('div')(({theme}) => ({
   display: 'flex',
   alignItems: 'center',
   padding: theme.spacing(2, 2.5),
@@ -35,56 +35,60 @@ Nav.propTypes = {
   onCloseNav: PropTypes.func,
 };
 
-export default function Nav({ openNav, onCloseNav }) {
-  const { pathname } = useLocation();
-  const { user: { name, email, image } } = useAuth()
-
+export default function Nav({openNav, onCloseNav}) {
+  const {pathname} = useLocation();
+  const {user: {name, email, image}} = useAuth()
+  
   const isDesktop = useResponsive('up', 'lg');
-
+  
   useEffect(() => {
     if (openNav) {
       onCloseNav();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
-
+  
   const renderContent = (
     <Scrollbar
       sx={{
         height: 1,
-        '& .simplebar-content': { height: 1, display: 'flex', flexDirection: 'column' },
+        '& .simplebar-content': {height: 1, display: 'flex', flexDirection: 'column'},
       }}
     >
-      <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
-        <Logo />
+      <Box sx={{px: 2.5, py: 3, display: 'inline-flex'}}>
+        <Logo/>
       </Box>
-
-      <Box sx={{ mb: 5, mx: 2.5 }}>
+      
+      <Box sx={{mb: 5, mx: 2.5}}>
         <Link underline="none">
           <StyledAccount>
-            <Avatar src={image || account.photoURL} alt="photoURL" />
-
-            <Box sx={{ ml: 2 }}>
-              <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
+            {
+              image
+                ? <Avatar src={image} alt="photoURL"/>
+                : <AccountCircleIcon/>
+            }
+            
+            <Box sx={{ml: 2}}>
+              <Typography variant="subtitle2" sx={{color: 'text.primary'}}>
                 {name || email}
               </Typography>
             </Box>
           </StyledAccount>
         </Link>
       </Box>
-
-      <NavSection data={navConfig} />
-
-      <Box sx={{ flexGrow: 1 }} />
+      
+      <NavSection data={navConfig}/>
+      
+      <Box sx={{flexGrow: 1}}/>
     </Scrollbar>
   );
-
+  
   return (
     <Box
       component="nav"
       sx={{
-        flexShrink: { lg: 0 },
-        width: { lg: NAV_WIDTH },
+        flexShrink: {lg: 0},
+        width: {lg: NAV_WIDTH},
       }}
     >
       {isDesktop ? (
@@ -109,7 +113,7 @@ export default function Nav({ openNav, onCloseNav }) {
             keepMounted: true,
           }}
           PaperProps={{
-            sx: { width: NAV_WIDTH },
+            sx: {width: NAV_WIDTH},
           }}
         >
           {renderContent}
