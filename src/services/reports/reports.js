3f8d88a9e9f566aa8176b36b209dcbeade17d717
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2023-01-10 10:18:13 -0300

    feat: add pagination and search for reports

--- src/services/reports/reports.js
+++ src/services/reports/reports.js
@@ -1,17 +1,62 @@
-import { collection, getDocs, doc, updateDoc, addDoc } from 'firebase/firestore'
+import {
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  addDoc,
+  query,
+  orderBy,
+  startAt,
+  endAt,
+  limit, startAfter
+} from 'firebase/firestore'
 import { db } from '../../utils/firebase'
 
 const reportsCollection = collection(db, "pagamentos")
 
-export const getReports = async () => {
+const pageLimit = 10
+
+const queryReportsByName = (name) => query(reportsCollection,
+  orderBy('nomeAluno'),
+  startAt(name),
+  endAt(`${name}\uf8ff`)
+);
+
+export const findReports = async (name) => {
   const reports = []
-  const querySnapshot = await getDocs(reportsCollection);
+  const querySnapshot = await getDocs(queryReportsByName(name));
   querySnapshot.forEach((doc) => {
-    reports.push({
-      id: doc.uid,
-      ...doc.data()
-    });
+    reports.push({id: doc.id, ...doc.data()});
   });
+  
+  return reports
+}
+
+export const getReportsPageCount = async () => {
+  const querySnapshot = await getDocs(reportsCollection);
+  return Math.ceil(querySnapshot.size / pageLimit)
+}
+
+export const getReportsPerPage = async (page) => {
+  const reports = []
+  const firstLimit = page === 1 ? page : (page - 1)
+  const first = query(reportsCollection, orderBy('nomeAluno'), limit(pageLimit * firstLimit))
+  const querySnapshot = await getDocs(first);
+  const lastReport = querySnapshot.docs[querySnapshot.docs.length - 1]
+  const next = query(reportsCollection, orderBy('nomeAluno'), startAfter(lastReport), limit(pageLimit))
+  
+  if (page === 1) {
+    querySnapshot.forEach((doc) => {
+      reports.push({id: doc.id, ...doc.data()});
+    });
+  } else {
+    const nextReport = await getDocs(next)
+    
+    nextReport.forEach((doc) => {
+      reports.push({id: doc.id, ...doc.data()});
+    });
+  }
+  
   return reports
 }
 
@@ -21,6 +66,7 @@ export const updateReport = async (report) => {
 
   await updateDoc(reportRef, report)
 }
+
 export const createReport = async (user,userId) =>{
   await addDoc(reportsCollection, {
     cpf:user?.documento,
